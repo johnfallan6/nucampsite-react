d23b024afc531913a755bf39de80799886bbec7b
John Allan <johnfallan6@gmail.com>
2022-03-06 12:23:04 -0600

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Card,
   CardImg,
@@ -8,68 +8,56 @@ import {
   CardTitle,
 } from "reactstrap";
 
-class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsite: null,
-    };
-  }
+function RenderCampsite({ campsite }) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
-  renderCampsite(campsite) {
+function RenderComments({ comments }) {
+  if (comments) {
     return (
       <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+        <h4>Comments</h4>
+        {comments.map((comment) => (
+          <div>
+            <p>{comment.text} <br></br>
+              {" "}
+              -- {comment.author},{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}{" "}
+            </p>
+          </div>
+        ))}
       </div>
     );
   }
+  return <div />;
+}
 
-  renderComments(comments) {
-    if (comments) {
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map((comment) => (
-            <div>
-              <p>{comment.text}</p>
-              <p>
-                {" "}
-                -- {comment.author},{" "}
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "2-digit",
-                }).format(new Date(Date.parse(comment.date)))}{" "}
-              </p>
-            </div>
-          ))}
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} />
         </div>
-      );
-    } else {
-      return <div />;
-    }
+      </div>
+    );
   }
-
-  render() {
-    if (this.props.campsite) {
-      return (
-          <div className="container">
-            <div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>
-        </div>
-      );
-    } else {
-      return <div />;
-    }
-  }
+  return <div />;
 }
 
 export default CampsiteInfo;
